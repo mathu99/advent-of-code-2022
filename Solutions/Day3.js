629c748cbfad5a5279312f4c2e4b907bfa642e47
Matthew Mills <mathu99@gmail.com>
2022-12-03 19:32:04 +0200

    day 3, part 2

--- Solutions/Day3.js
+++ Solutions/Day3.js
@@ -17,14 +17,35 @@ fs.readFile('./Input/Day3.txt', (err, data) => {
         totalScore += getMatch(firstHalf, secondHalf);
     });
 
-    console.log(totalScore);
+    let rowCount = 0, part2Score = 0;
+    lines.forEach(line => {
+        rowCount++;
+        if (rowCount % 3 === 0) {
+            part2Score += getMatchLines(lines[rowCount - 1], lines[rowCount - 2], lines[rowCount - 3]);
+        }
+    });
+
+    console.log('Part 1: ', totalScore);
+    console.log('Part 2: ', part2Score);
 });
 
 const getMatch = (firstHalf, secondHalf) => {
-    console.log(firstHalf, secondHalf)
     for (let i = 0; i < firstHalf.length; i++) {
         let char = firstHalf[i];
         if (secondHalf.indexOf(char) > -1) {
+            if (lowercase.indexOf(char) > -1) {
+                return lowercase.indexOf(char) + 1;
+            } else {
+                return uppercase.indexOf(char) + 27;
+            }
+        }
+    }
+}
+
+const getMatchLines = (firstPart, secondPart, thirdPart) => {
+    for (let i = 0; i < firstPart.length; i++) {
+        let char = firstPart[i];
+        if (secondPart.indexOf(char) > -1 && thirdPart.indexOf(char) > -1) {
             if (lowercase.indexOf(char) > -1) {
                 return lowercase.indexOf(char) + 1;
             } else {
